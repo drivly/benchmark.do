aca187a09766c0964aff7cf9c37e5e2651595031
Nathan Clevenger <nateclev@gmail.com>
2022-08-25 07:01:53 -0500

    Create worker.js

---
+++ worker.js
@@ -0,0 +1,24 @@
+export default {
+  fetch: async req => {
+    
+    const { pathname } = new URL(req.url)
+    let perf = []
+    
+    for (i = 0, i++ i < 20) {
+      const startTime = Date.now()
+      const data = await fetch('https:/' + pathname + search, req))
+      perf.push(startTime - Date.now())
+    }
+ 
+    return new Response(JSON.stringify({
+      target: 'https:/' + pathname + search,
+      first: perf[0],
+      min: perf.sort()[0],
+      max: perf.sort()[19],
+      avg: perf.reduce((acc, x) => acc + x, 0),
+      med: perf.sort()[10],
+      p25: perf.sort()[5],
+      p75: perf.sort()[15],
+    }, null, 2), { headers: { 'content-type': 'application/json' }})
+  }
+}
