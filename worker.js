f977708124f71eeada524ecdcede8ce38f98cc4e
Nathan Clevenger <nateclev@gmail.com>
2022-08-25 07:53:12 -0500

    Update worker.js

--- worker.js
+++ worker.js
@@ -4,7 +4,7 @@ export default {
     const { pathname, search } = new URL(req.url)
     let perf = []
     
-    for (let i = 0; i <= 20; i++) {
+    for (let i = 0; i < 20; i++) {
       const startTime = Date.now()
       const data = await fetch('https:/' + pathname + search, req)
       const time = Date.now() - startTime
@@ -15,10 +15,10 @@ export default {
       target: 'https:/' + pathname + search,
       first: perf[0],
       min: perf.sort()[0],
-      max: perf.sort()[20],
-      avg: (perf.reduce((acc, x) => acc + x, 0)) / 21,
-      med: perf.sort()[10],
-      p25: perf.sort()[5],
+      max: perf.sort()[19],
+      avg: (perf.reduce((acc, x) => acc + x, 0)) / 20,
+      med: perf.sort()[9],
+      p25: perf.sort()[4],
       p75: perf.sort()[15],
     }, null, 2), { headers: { 'content-type': 'application/json' }})
   }
